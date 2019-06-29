e9fe9274581387ea047e52de04f76fe7d3efe20a
Paula Illanes <illanesrpaula@gmail.com>
2019-06-29 16:38:12 -0300

    Commiteando

---
+++ ejercicio1.js
@@ -0,0 +1,22 @@
+
+
+const precioMaquina = componentes => {
+    const precioComponentes = precio => precio.precio === componentes;
+    const filtrarPrecios;
+
+    return filtrarPrecios;
+}
+
+console.log(precioMaquina('Monitor GPRS 3000', 'Motherboard ASUS 1500'));
+
+const vendedoraDelMes = (mes, anio) => {
+
+}
+
+const ventasVenderora = nombre => {
+
+}
+
+const huboVentas = (mes, anio) => {
+
+}
