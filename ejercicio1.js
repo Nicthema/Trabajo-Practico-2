f27e8c18c3839e2adcee663c25750114b67affb9
Nicthema <illanesrpaula@gmail.com>
2019-07-04 13:59:26 -0300

    Agregando

--- ejercicio1.js
+++ ejercicio1.js
@@ -1,37 +1,83 @@
-
-// const precioMaquina = componente => {
-//     let precioTotal = 0;
-//     const precio = precioComponente => precioComponente.componente === componente;
-//     const precioFilter = local.precios.filter(precio);
-//     precioTotal+= local.precios.componente;
-
-//     return precioTotal;
-// }
-
-let componente = [];
+// 1. precioMaquina(); Peu
 
 const precioMaquina = (componente) => {
     let precioTotal = 0;
-    for(const component of componente) {
-        for(const precio of local.precios) {
-            if(component === precio.componente) {
-            precioTotal += precio.precio;
+    for (const component of componente) {
+        for (const precio of local.precios) {
+            if (component === precio.componente) {
+                precioTotal += precio.precio;
             }
         }
     }
-    
-  return precioTotal;
+
+    return precioTotal;
 }
 
 console.log(precioMaquina(['Motherboard ASUS 1500', 'Motherboard ASUS 1500']));
 
-const vendedoraDelMes = (mes, anio) => {
+// 2. cantidadVentasComponente(); Leri
 
+// const cantidadVentasComponente = componente => {
+//     let cantidadComponentes = 0;
+
+//     for (const venta of local.ventas) {
+//       for (const comp of venta.componentes) {
+//         if (componente === comp) {
+//           cantidadComponentes++;
+//         }
+//       }
+//     }
+
+//     return cantidadComponentes;
+//   }
+
+// 3. vendedoraDelMes(); Peu
+
+const ventasMes = (mes, anio) => {
+    let ventasUnMes = 0;
+    const ventasPorMes = local.ventas.filter(venta => venta.fecha.getMonth() === mes - 1 && venta.fecha.getFullYear() === anio)
+    for (const venta of ventasPorMes) {
+        ventasUnMes += precioMaquina(venta.componentes);
+    }
+    return ventasUnMes;
 }
 
-const ventasVenderora = nombre => {
+console.log(vendedoraDelMes(1, 2019)); 
 
+// 4. ventasMes(); Leri
+
+// 5. ventasVendedora(); Peu
+
+const ventasVendedora = nombre => {
+    let total = 0;
+    for(const venta of local.ventas) {
+        if(venta.nombreVendedora === nombre) {
+            total+= precioMaquina(venta.componentes);
+        }
+        
+    }
+    return total;
 }
+
+console.log(ventasVendedora('Grace'));
+
+// 6. componenteMasVendido(); Leri
+
+// const componenteMasVendido = () => {
+//     let componenteActual = "";
+
+//     for (const venta of local.ventas) {
+//       for (const componente of venta.componentes) {
+//         if (cantidadVentasComponente(componente) > cantidadVentasComponente(componenteActual)) {
+//           componenteActual = componente;
+//         }
+//       }
+//     }
+
+//     return componenteActual;
+//   }
+
+// 7. huboVentas(); Peu && Leri
 
 const huboVentas = (mes, anio) => {
 
